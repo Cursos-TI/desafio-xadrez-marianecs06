b1b537736ad960e1e5e896c3725b46f5f00c8fa2
marianecs06 <marianecs@outlook.com>
2026-01-11 04:09:22 +0000

    nivelnovato

--- xadrez.c
+++ xadrez.c
@@ -5,17 +5,56 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+   
+    // Variáveis para o controle do número de movimentos
+    int torre = 5; // Torre move 5 casas para a direita
+    int bispo = 5; // Bispo move 5 casas para a diagonal superior direita
+    int rainha = 8; // Rainha move 8 casas para a esquerda
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    int contador; // Variável auxiliar para os loops
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("\n === Simulação de movimentos de xadrez ===\n");
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // Movimento da Torre (estrutura FOR)
+    printf("\n 1. Movimento da Torre\n");
+    printf("Direção: Direita\n");
+    printf("Casas a percorrer: %d\n", torre);
+    printf("Trajetória:\n");
+
+    for (contador = 1; contador <= torre; contador++)
+    {
+        printf("Casa %d: Direita\n", contador);
+    }
+    
+    // Movimento do Bispo (estrutura WHILE)
+    printf("\n 2. Movimento do Bispo\n");
+    printf("Direção: Diagonal superior direita\n");
+    printf("Casas a percorrer: %d\n", bispo);
+    printf("Trajetória:\n");
+
+    contador = 1; // Reinicia o contador
+    while (contador <= bispo)
+    {
+        printf("Casa %d: Cima, Direita\n", contador);
+       
+        contador++; // Incrementa o contador
+    }
+
+    // Movimento da Rainha (estrutura DO-WHILE)
+    printf("\n 3. Movimento da Rainha\n");
+    printf("Direção: Esquerda\n");
+    printf("Casas a percorrer: %d\n", rainha);
+    printf("Trajetória:\n");
+    
+    contador = 1; // Reinicia o contador novamente
+    do
+    {
+        printf("Casa %d: Esquerda\n", contador);
+        contador++; // Incrementa o contador
+    } while (contador <= rainha);
+
+    printf("\n Fim da simulação.\n");
+    
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
